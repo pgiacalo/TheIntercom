e57d4448194b66993dae23ab85823156a6dcd4df
Philip Giacalone <philip_giacalone@yahoo.com>
2023-10-07 14:10:34 -0700

    Added text document summarizing this application. Added logic to switch gpio pins, based on which ESP32 is targeted via menuconfig settings.

--- main/bluetooth_config.h
+++ main/bluetooth_config.h
@@ -1,5 +1,7 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef BLUETOOTH_CONFIG_H
+#define BLUETOOTH_CONFIG_H
+
+#include "sdkconfig.h"
 
 //PHIL added this config file to set the role and the mac addresses
 
@@ -12,25 +14,48 @@
 
 // MAC Addresses of bluetooth earbuds
 #if DEVICE_ROLE == ROLE_MASTER
-#define HF_PEER_ADDR {0x58, 0xFC, 0xC6, 0x6C, 0x0A,0x03}		//TOZO Mazda earbuds
+    #define HF_PEER_ADDR {0x58, 0xFC, 0xC6, 0x6C, 0x0A,0x03}		//TOZO Mazda earbuds
 #elif DEVICE_ROLE == ROLE_SLAVE
-#define HF_PEER_ADDR {0x54, 0xB7, 0xE5, 0x8C, 0x07,0x71}		//TOZO Home earbuds
-// #define HF_PEER_ADDR {0x74, 0x74, 0x46, 0xED, 0x07, 0x6B}		//Pixel Buds Series A  
+    #define HF_PEER_ADDR {0x54, 0xB7, 0xE5, 0x8C, 0x07,0x71}		//TOZO Home earbuds
+    // #define HF_PEER_ADDR {0x74, 0x74, 0x46, 0xED, 0x07, 0x6B}		//Pixel Buds Series A  
 #endif
 
-// Pin configurations for ESP32 Master
-#define MASTER_GPIO_DIN         14
-#define MASTER_GPIO_DOUT        25
-#define MASTER_GPIO_BCLK        26	//was 5 (green)
-#define MASTER_GPIO_LRC         27	//was 4 (blue)
 
-// Pin configurations for ESP32 Slave
-#define SLAVE_GPIO_DOUT         25
-#define SLAVE_GPIO_DIN          14
-#define SLAVE_GPIO_BCLK         27	//was 17 (green)
-#define SLAVE_GPIO_LRC          26	//was 19 (blue)
+//setup the ESP32 target in menuconfig
+#if CONFIG_IDF_TARGET_ESP32         
 
-#endif // CONFIG_H
+    // Pin configurations for ESP32 Master
+    #define MASTER_GPIO_DIN         14
+    #define MASTER_GPIO_DOUT        25
+    #define MASTER_GPIO_BCLK        26  //was 5 (green)
+    #define MASTER_GPIO_LRC         27  //was 4 (blue)
+
+    // Pin configurations for ESP32 Slave
+    #define SLAVE_GPIO_DOUT         25
+    #define SLAVE_GPIO_DIN          14
+    #define SLAVE_GPIO_BCLK         27  //was 17 (green)
+    #define SLAVE_GPIO_LRC          26  //was 19 (blue)
+
+#elif CONFIG_IDF_TARGET_ESP32S3
+
+    // Pin configurations for ESP32-S3 Master (Modify pins as needed)
+    #define MASTER_GPIO_DIN         /* Your S3 Pin */
+    #define MASTER_GPIO_DOUT        /* Your S3 Pin */
+    #define MASTER_GPIO_BCLK        /* Your S3 Pin */
+    #define MASTER_GPIO_LRC         /* Your S3 Pin */
+
+    // Pin configurations for ESP32-S3 Slave (Modify pins as needed)
+    #define SLAVE_GPIO_DOUT         /* Your S3 Pin */
+    #define SLAVE_GPIO_DIN          /* Your S3 Pin */
+    #define SLAVE_GPIO_BCLK         /* Your S3 Pin */
+    #define SLAVE_GPIO_LRC          /* Your S3 Pin */
+
+#else
+    #error "Unsupported IDF target (see bluetooth_config.h)"
+#endif 
+
+#endif // BLUETOOTH_CONFIG_H
+
 
 /**************************************
 //PHIL added this note about the "standard" ESP32 I2S pin numbers (but pin choices are flexible)
